c83b4c4925afb01ae98e5f77d9873f5f14caabfb
tomvancauwenberghe <tom.vancauwenberghe@cegeka.com>
2022-07-15 13:35:30 +0200

    add tests

---
+++ PipelineDemo/PipelineDemo.Tests/PipelineTests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace PipelineDemo.Tests
+{
+    public class PipelineTests
+    {
+        [Fact]
+        public void TestDemo()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
